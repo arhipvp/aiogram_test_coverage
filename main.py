6238d78c86913721be50b51ae26d637657519f02
Архипов Владимир <arhipvp@gmail.com>
2023-08-13 15:53:11 +0300

    13/08/2023

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-from aiogram import Bot, Dispatcher, types
+import asyncio
+import logging
+
 import my_bot
 
 
+async def main() -> None:
+    await my_bot.dp.start_polling(my_bot.bot)
 
-# Обработчик команды /start
-@dp.message_handler(commands=["start"])
-async def cmd_start(message: types.Message):
-    await message.reply("Привет! Я эхо-бот. Просто отправь мне сообщение, и я повторю его.")
 
-# Эхо-обработчик для всех сообщений
-@dp.message_handler()
-async def echo_message(message: types.Message):
-    await message.answer(message.text)
-
-# Запускаем бота
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
--- my_bot.py
+++ my_bot.py
@@ -1,10 +1,30 @@
 import os
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+from aiogram.types import Message
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types
 
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
-bot = Bot(TELEGRAM_TOKEN)
-dp = Dispatcher(bot)
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+router = Router()
+
+dp = Dispatcher()
+dp.include_router(router)
+bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+
+
+@router.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, <b>{message.from_user.full_name}!</b>")
+
+
+@router.message()
+async def echo_handler(message: types.Message) -> None:
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer("Nice try!")
+
